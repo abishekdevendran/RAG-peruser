12e9ca436a8e1b00a1759e7dd40be7e63073834d
Abishek Devendran <abishekdevendran@gmail.com>
2024-01-15 17:33:55 +0530

    Fixed a problem

--- qa.py
+++ qa.py
@@ -4,12 +4,8 @@ load_dotenv(find_dotenv())
 from langchain_community.llms import Ollama
 llm = Ollama(model="llama2", temperature=0.9)
 
-import os
-from langchain_community.utilities import SQLDatabase
-DB_URL = os.getenv("SQL_URL");
-db = SQLDatabase.from_uri(DB_URL)
+# import calculator and duckduckgo tools
+from langchain_community.tools import calculator, duckduckgo
 
-from langchain.chains import create_sql_query_chain
-chain = create_sql_query_chain(llm, db)
-response = chain.invoke({"question": "How many posts are there"})
-print(response)
+# initialize tools
+tools = [calculator, duckduckgo]
